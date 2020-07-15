76491b51593814e6a3376c5810ff5482ccf506ac
mikdadayan <dadayan.mikayel@gmail.com>
2020-07-15 12:52:12 +0400

    updating our app to redirect home if user is signed in, added to our app redux, react-redux, redux-logger

--- src/App.js
+++ src/App.js
@@ -1,64 +1,74 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from './components/header/header.component'
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
 import SignInAndSignUp from "./pages/sign-in-and-signup.component/sign-in-and-signup.component";
-import { auth, createUserProfileDocument } from './components/firebase/firebase.utils';
+import {
+  auth,
+  createUserProfileDocument,
+} from "./components/firebase/firebase.utils";
+import { connect } from "react-redux";
 
+import Header from "./components/header/header.component";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import { setCurrentUser } from "./components/redux/user/user-action";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
-
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
-        userRef.onSnapshot(async snapshot => {
-          console.log(snapshot.data())
-          this.setState({
-            currentUser: {
-              id: snapshot.id,
-              ...(await snapshot.data())
-            } 
-          }, () => {
-            console.log(this.state)
-
-          })
-        })
-        console.log(this.state)
+        userRef.onSnapshot(async (snapshot) => {
+          console.log("88888", snapshot.data());
+          this.props.setCurrentUser(
+            {
+                id: snapshot.id,
+                ...(await snapshot.data()),
+              },
+            () => {
+              console.log("$$$$$", this.state);
+            }
+          );
+        });
+        console.log("#####", this.state);
+      } else {
+        this.props.setCurrentUser(userAuth);
       }
-      else {
-        this.setState({currentUser: userAuth})
-      }
-    })
-    
+    });
   }
 
   componentWillUnmount() {
-    this.unsubscribeFromAuth()
+    this.unsubscribeFromAuth();
   }
 
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' component={SignInAndSignUp} />
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUp />
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
+const mapDispatchToState = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToState)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import './header.styles.scss'
-import { auth } from '../firebase/firebase.utils';
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
-const Header = ({currentUser}) => (
-    <div className='header'>
-        <Link className='logo-container' to='/'>
-            <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link to='/shop' className='option'>SHOP</Link>
-            <Link to='/contact' className='option'>CONTACT</Link>
-            { currentUser ? 
-                <div className='option' onClick={ () => auth.signOut()}>SIGN OUT</div> :
-                <Link className='option' to='/signin'>SIGN IN</Link>
-            }
+import "./header.styles.scss";
+import { auth } from "../firebase/firebase.utils";
+
+const Header = ({ currentUser }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link to="/shop" className="option">
+        SHOP
+      </Link>
+      <Link to="/contact" className="option">
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
     </div>
-)
+  </div>
+);
 
-export default Header;
+const mapStateToProps = (state) => ({
+    currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
